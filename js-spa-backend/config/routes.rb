10db3cfd303377fc51751c967b9b375cf4e4478f
msteele96 <marcsteele2014@gmail.com>
2021-06-03 16:52:23 -0400

    fixed routes

--- js-spa-backend/config/routes.rb
+++ js-spa-backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :scores only: [:index, :show, :create]
-  resources :users only: [:index, :show, :create]
+  resources :scores, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create]
   get '/highscores', to: 'scores#high'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
